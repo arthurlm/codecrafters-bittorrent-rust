0979bd7028772948ed82fe5256da5fdd5906b380
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-15 14:13:13 +0100

    Add download command

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,11 @@ enum Commands {
         meta_info_path: PathBuf,
         piece_id: u32,
     },
+    Download {
+        #[arg(short = 'o')]
+        output_path: PathBuf,
+        meta_info_path: PathBuf,
+    },
 }
 
 #[tokio::main]
@@ -92,10 +97,29 @@ async fn main() {
             piece_id,
             ..
         } => {
-            let contents = download_piece(meta_info_path, piece_id)
+            let meta_info = read_file(meta_info_path);
+            let mut peer = connect_any_peer_addr(&meta_info)
+                .await
+                .expect("Cannot connect to peer");
+            let contents = download_piece(&meta_info, &mut peer, piece_id)
                 .await
                 .expect("Fail to download msg piece");
             fs::write(output_path, contents).expect("Fail to write data to disk");
+        }
+        Commands::Download {
+            output_path,
+            meta_info_path,
+        } => {
+            let meta_info = read_file(meta_info_path.clone());
+            let mut peer = connect_any_peer_addr(&meta_info)
+                .await
+                .expect("Cannot connect to peer");
+            let contents = download(&meta_info, &mut peer)
+                .await
+                .expect("Fail to download file");
+            fs::write(&output_path, contents).expect("Fail to write data to disk");
+
+            println!("Downloaded {meta_info_path:?} to {output_path:?}.")
         }
     }
 }
@@ -106,9 +130,8 @@ fn read_file(path: PathBuf) -> MetaInfoFile {
     serde_json::from_value(decoded_value.into()).expect("Fail to decode torrent file")
 }
 
-async fn download_piece(meta_info_path: PathBuf, piece_id: u32) -> Result<Vec<u8>, TorrentError> {
+async fn connect_any_peer_addr(meta_info: &MetaInfoFile) -> Result<Peer, TorrentError> {
     // Query tracker
-    let meta_info = read_file(meta_info_path);
     let tracker_response = trackers::query(&meta_info).await?;
 
     // Take 1st peer
@@ -136,6 +159,14 @@ async fn download_piece(meta_info_path: PathBuf, piece_id: u32) -> Result<Vec<u8
         eprintln!("Received unexpected message: {msg:?}");
     }
 
+    Ok(peer)
+}
+
+async fn download_piece(
+    meta_info: &MetaInfoFile,
+    peer: &mut Peer,
+    piece_id: u32,
+) -> Result<Vec<u8>, TorrentError> {
     // Send request for each piece and wait for its response.
     // TODO: Add pipelining
     const CHUNK_SIZE: u32 = 16 << 10;
@@ -194,4 +225,16 @@ async fn download_piece(meta_info_path: PathBuf, piece_id: u32) -> Result<Vec<u8
     assert_eq!(contents_hash.encode_hex::<String>(), *expected_hash);
 
     Ok(contents)
+}
+
+async fn download(meta_info: &MetaInfoFile, peer: &mut Peer) -> Result<Vec<u8>, TorrentError> {
+    let piece_count = meta_info.info.pieces_count();
+    let mut pieces = Vec::with_capacity(piece_count);
+
+    for piece_id in 0..piece_count {
+        let piece_content = download_piece(meta_info, peer, piece_id as u32).await?;
+        pieces.push(piece_content);
+    }
+
+    Ok(pieces.into_iter().flatten().collect())
 }
